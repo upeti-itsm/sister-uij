436e32b993c615c9cb1e3bba9ca5430f2241cc63
Muhammad Mauribi <muhampedia.id@gmail.com>
2025-08-23 20:19:30 +0700

    Add option for the year 2025 in various honorarium and salary deduction views

--- resources/views/keuangan_page/penggajian/daftar_gaji_bulanan.blade.php
+++ resources/views/keuangan_page/penggajian/daftar_gaji_bulanan.blade.php
@@ -1,8 +1,8 @@
 @extends('sidebar')
 @section('head-css')
-    <link href="{{asset('adminpage/assets/plugins/datatables/datatables.min.css')}}" rel="stylesheet">
-    <link href="{{asset('adminpage/assets/plugins/select2/css/select2.min.css')}}" rel="stylesheet">
-    <link href="{{asset('adminpage/assets/plugins/select2/css/select2-bootstrap4.min.css')}}" rel="stylesheet">
+    <link href="{{ asset('adminpage/assets/plugins/datatables/datatables.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('adminpage/assets/plugins/select2/css/select2.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('adminpage/assets/plugins/select2/css/select2-bootstrap4.min.css') }}" rel="stylesheet">
 @endsection
 @section('content-header')
     <nav aria-label="breadcrumb" class="col-sm-4 order-sm-last mb-3 mb-sm-0 p-0 ">
@@ -37,7 +37,7 @@
                         <div class="float-right">
                             <button class="btn btn-primary" id="btn_buat_gaji" title="Buat Gaji Bulan Ini">
                                 <span class='spinner-border spinner-border-sm mr-2' id='loading-spin-buat-gaji'
-                                      style='display: none' role='status' aria-hidden='true'></span><i
+                                    style='display: none' role='status' aria-hidden='true'></span><i
                                     class="fas fa-plus-square"></i> Buat Gaji
                             </button>
                         </div>
@@ -54,7 +54,7 @@
                                     <div class="row">
                                         <div class="col-md-8">
                                             <input type="text" class="form-control" placeholder="Cari Nama Karyawan"
-                                                   id="cari-data">
+                                                id="cari-data">
                                         </div>
                                         <div class="col-md-4">
                                             <button class="btn btn-block btn-primary" id="btn-cari-data"><i
@@ -71,22 +71,35 @@
                                         <div class="col-md-4">
                                             <select class="select2 form-control" id="filter_bulan">
                                                 <option value="-1">-- Semua Bulan --</option>
-                                                <option value="1" @if($bulan == 1) selected @endif>Januari</option>
-                                                <option value="2" @if($bulan == 2) selected @endif>Februari</option>
-                                                <option value="3" @if($bulan == 3) selected @endif>Maret</option>
-                                                <option value="4" @if($bulan == 4) selected @endif>April</option>
-                                                <option value="5" @if($bulan == 5) selected @endif>Mei</option>
-                                                <option value="6" @if($bulan == 6) selected @endif>Juni</option>
-                                                <option value="7" @if($bulan == 7) selected @endif>Juli</option>
-                                                <option value="8" @if($bulan == 8) selected @endif>Agustus</option>
-                                                <option value="9" @if($bulan == 9) selected @endif>September</option>
-                                                <option value="10" @if($bulan == 10) selected @endif>Oktober</option>
-                                                <option value="11" @if($bulan == 11) selected @endif>November</option>
-                                                <option value="12" @if($bulan == 12) selected @endif>Desember</option>
+                                                <option value="1" @if ($bulan == 1) selected @endif>
+                                                    Januari</option>
+                                                <option value="2" @if ($bulan == 2) selected @endif>
+                                                    Februari</option>
+                                                <option value="3" @if ($bulan == 3) selected @endif>
+                                                    Maret</option>
+                                                <option value="4" @if ($bulan == 4) selected @endif>
+                                                    April</option>
+                                                <option value="5" @if ($bulan == 5) selected @endif>Mei
+                                                </option>
+                                                <option value="6" @if ($bulan == 6) selected @endif>
+                                                    Juni</option>
+                                                <option value="7" @if ($bulan == 7) selected @endif>
+                                                    Juli</option>
+                                                <option value="8" @if ($bulan == 8) selected @endif>
+                                                    Agustus</option>
+                                                <option value="9" @if ($bulan == 9) selected @endif>
+                                                    September</option>
+                                                <option value="10" @if ($bulan == 10) selected @endif>
+                                                    Oktober</option>
+                                                <option value="11" @if ($bulan == 11) selected @endif>
+                                                    November</option>
+                                                <option value="12" @if ($bulan == 12) selected @endif>
+                                                    Desember</option>
                                             </select>
                                         </div>
                                         <div class="col-md-4">
                                             <select class="select2 form-control" id="filter_tahun">
+                                                <option value="2025">2025</option>
                                                 <option value="2024">2024</option>
                                                 <option value="2023">2023</option>
                                                 <option value="2022">2022</option>
@@ -103,10 +116,10 @@
                             </div>
                             <div class="col-md-2">
                                 <div class="form-group">
-                                    <label class="font-weight-bold">&nbsp;</label><br/>
+                                    <label class="font-weight-bold">&nbsp;</label><br />
                                     <div class="float-right">
-                                        <button class="btn btn-success mr-2" title="Export ke Excel" id="btn_export_excel"><i
-                                                class="fas fa-file-excel"></i>
+                                        <button class="btn btn-success mr-2" title="Export ke Excel"
+                                            id="btn_export_excel"><i class="fas fa-file-excel"></i>
                                         </button>
                                         <button class="btn btn-danger mr-2" title="Export ke PDF" id="btn_export_pdf"><i
                                                 class="fas fa-file-pdf"></i>
@@ -121,12 +134,12 @@
                     <div class="table-responsive">
                         <table class="table table-striped table-bordered table-hover" id="table">
                             <thead>
-                            <tr>
-                                <th class="text-center">Nomor</th>
-                                <th class="text-left">Identitas Pegawai</th>
-                                <th class="text-left">Gaji</th>
-                                <th class="text-center"><i class="fas fa-th"></i></th>
-                            </tr>
+                                <tr>
+                                    <th class="text-center">Nomor</th>
+                                    <th class="text-left">Identitas Pegawai</th>
+                                    <th class="text-left">Gaji</th>
+                                    <th class="text-center"><i class="fas fa-th"></i></th>
+                                </tr>
                             </thead>
                             <tbody></tbody>
                         </table>
@@ -139,7 +152,7 @@
 @section('modal')
 @endsection
 @push('scripts')
-    <script src="{{asset('adminpage/assets/plugins/datatables/datatables.min.js')}}"></script>
-    <script src="{{asset('adminpage/assets/plugins/select2/js/select2.min.js')}}"></script>
-    <script src="{{asset('adminpage/own-js/keuangan_page/penggajian/daftar_gaji_bulanan.js')}}"></script>
+    <script src="{{ asset('adminpage/assets/plugins/datatables/datatables.min.js') }}"></script>
+    <script src="{{ asset('adminpage/assets/plugins/select2/js/select2.min.js') }}"></script>
+    <script src="{{ asset('adminpage/own-js/keuangan_page/penggajian/daftar_gaji_bulanan.js') }}"></script>
 @endpush
--- resources/views/keuangan_page/penggajian/honorarium/honorarium_koreksi/daftar.blade.php
+++ resources/views/keuangan_page/penggajian/honorarium/honorarium_koreksi/daftar.blade.php
@@ -1,8 +1,8 @@
 @extends('sidebar')
 @section('head-css')
-    <link href="{{asset('adminpage/assets/plugins/datatables/datatables.min.css')}}" rel="stylesheet">
-    <link href="{{asset('adminpage/assets/plugins/select2/css/select2.min.css')}}" rel="stylesheet">
-    <link href="{{asset('adminpage/assets/plugins/select2/css/select2-bootstrap4.min.css')}}" rel="stylesheet">
+    <link href="{{ asset('adminpage/assets/plugins/datatables/datatables.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('adminpage/assets/plugins/select2/css/select2.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('adminpage/assets/plugins/select2/css/select2-bootstrap4.min.css') }}" rel="stylesheet">
 @endsection
 @section('content-header')
     <nav aria-label="breadcrumb" class="col-sm-4 order-sm-last mb-3 mb-sm-0 p-0 ">
@@ -31,17 +31,18 @@
                             <div>
                                 <h6 class="fs-17 font-weight-600 mb-0">Daftar Honorarium Koreksi</h6>
                             </div>
-                            @if(array_key_exists("Pengelolaan Honorarium Mengajar", \Illuminate\Support\Facades\Session::get('modul')) || array_key_exists("Pengelolaan Honorarium Mengajar", \Illuminate\Support\Facades\Session::get('modul')))
+                            @if (array_key_exists('Pengelolaan Honorarium Mengajar', \Illuminate\Support\Facades\Session::get('modul')) ||
+                                    array_key_exists('Pengelolaan Honorarium Mengajar', \Illuminate\Support\Facades\Session::get('modul')))
                                 <div class="text-right">
                                     <div class="actions">
                                         <div class="dropdown action-item" data-toggle="dropdown" aria-expanded="true">
                                             <a href="#" class="action-item">Honorarium Koreksi <i
                                                     class="fas fa-bars fa-fw"></i></a>
                                             <div class="dropdown-menu dropdown-menu-right" id="sub-menu">
-                                                <a href="{{route('keuangan.penggajian.honorarium.honorarium_mengajar.index')}}"
-                                                   class="dropdown-item">Honorarium Mengajar</a>
-                                                <a href="{{route('keuangan.penggajian.honorarium.honorarium_pengawas.index')}}"
-                                                   class="dropdown-item">Honorarium Pengawas</a>
+                                                <a href="{{ route('keuangan.penggajian.honorarium.honorarium_mengajar.index') }}"
+                                                    class="dropdown-item">Honorarium Mengajar</a>
+                                                <a href="{{ route('keuangan.penggajian.honorarium.honorarium_pengawas.index') }}"
+                                                    class="dropdown-item">Honorarium Pengawas</a>
                                             </div>
                                         </div>
                                     </div>
@@ -61,7 +62,7 @@
                                     <div class="row">
                                         <div class="col-md-8">
                                             <input type="text" class="form-control" placeholder="Cari Nama Karyawan"
-                                                   id="cari-data">
+                                                id="cari-data">
                                         </div>
                                         <div class="col-md-4">
                                             <button class="btn btn-block btn-primary" id="btn-cari-data"><i
@@ -78,22 +79,35 @@
                                         <div class="col-md-6">
                                             <select class="select2 form-control" id="filter_bulan">
                                                 <option value="-1">-- Semua Bulan --</option>
-                                                <option value="1" @if($bulan == 1) selected @endif>Januari</option>
-                                                <option value="2" @if($bulan == 2) selected @endif>Februari</option>
-                                                <option value="3" @if($bulan == 3) selected @endif>Maret</option>
-                                                <option value="4" @if($bulan == 4) selected @endif>April</option>
-                                                <option value="5" @if($bulan == 5) selected @endif>Mei</option>
-                                                <option value="6" @if($bulan == 6) selected @endif>Juni</option>
-                                                <option value="7" @if($bulan == 7) selected @endif>Juli</option>
-                                                <option value="8" @if($bulan == 8) selected @endif>Agustus</option>
-                                                <option value="9" @if($bulan == 9) selected @endif>September</option>
-                                                <option value="10" @if($bulan == 10) selected @endif>Oktober</option>
-                                                <option value="11" @if($bulan == 11) selected @endif>November</option>
-                                                <option value="12" @if($bulan == 12) selected @endif>Desember</option>
+                                                <option value="1" @if ($bulan == 1) selected @endif>
+                                                    Januari</option>
+                                                <option value="2" @if ($bulan == 2) selected @endif>
+                                                    Februari</option>
+                                                <option value="3" @if ($bulan == 3) selected @endif>
+                                                    Maret</option>
+                                                <option value="4" @if ($bulan == 4) selected @endif>
+                                                    April</option>
+                                                <option value="5" @if ($bulan == 5) selected @endif>Mei
+                                                </option>
+                                                <option value="6" @if ($bulan == 6) selected @endif>
+                                                    Juni</option>
+                                                <option value="7" @if ($bulan == 7) selected @endif>
+                                                    Juli</option>
+                                                <option value="8" @if ($bulan == 8) selected @endif>
+                                                    Agustus</option>
+                                                <option value="9" @if ($bulan == 9) selected @endif>
+                                                    September</option>
+                                                <option value="10" @if ($bulan == 10) selected @endif>
+                                                    Oktober</option>
+                                                <option value="11" @if ($bulan == 11) selected @endif>
+                                                    November</option>
+                                                <option value="12" @if ($bulan == 12) selected @endif>
+                                                    Desember</option>
                                             </select>
                                         </div>
                                         <div class="col-md-6">
                                             <select class="select2 form-control" id="filter_tahun">
+                                                <option value="2025">2025</option>
                                                 <option value="2024">2024</option>
                                                 <option value="2023">2023</option>
                                                 <option value="2022">2022</option>
@@ -104,17 +118,16 @@
                             </div>
                             <div class="col-md-4">
                                 <div class="form-group">
-                                    <label class="font-weight-bold">&nbsp;</label><br/>
+                                    <label class="font-weight-bold">&nbsp;</label><br />
                                     <div class="float-right">
                                         <button class="btn btn-primary mr-2" id="btn_buat_honor"
-                                                title="Buat Honor Koreksi Bulan Ini">
-                                <span class='spinner-border spinner-border-sm mr-2' id='loading-spin-buat-honor'
-                                      style='display: none' role='status' aria-hidden='true'></span><i
+                                            title="Buat Honor Koreksi Bulan Ini">
+                                            <span class='spinner-border spinner-border-sm mr-2' id='loading-spin-buat-honor'
+                                                style='display: none' role='status' aria-hidden='true'></span><i
                                                 class="fas fa-plus-square"></i> Buat Honorarium
                                         </button>
                                         <button class="btn btn-success mr-2" title="Export ke Excel"
-                                                id="btn_export_excel"><i
-                                                class="fas fa-file-excel"></i>
+                                            id="btn_export_excel"><i class="fas fa-file-excel"></i>
                                         </button>
                                         <button class="btn btn-danger mr-2" title="Export ke PDF" id="btn_export_pdf"><i
                                                 class="fas fa-file-pdf"></i>
@@ -129,12 +142,12 @@
                     <div class="table-responsive">
                         <table class="table table-striped table-bordered table-hover" id="table">
                             <thead>
-                            <tr>
-                                <th class="text-center">Nomor</th>
-                                <th class="text-left">Identitas Pegawai</th>
-                                <th class="text-left">Honorarium</th>
-                                <th class="text-center"><i class="fas fa-th"></i></th>
-                            </tr>
+                                <tr>
+                                    <th class="text-center">Nomor</th>
+                                    <th class="text-left">Identitas Pegawai</th>
+                                    <th class="text-left">Honorarium</th>
+                                    <th class="text-center"><i class="fas fa-th"></i></th>
+                                </tr>
                             </thead>
                             <tbody></tbody>
                         </table>
@@ -147,8 +160,7 @@
 @section('modal')
 @endsection
 @push('scripts')
-    <script src="{{asset('adminpage/assets/plugins/datatables/datatables.min.js')}}"></script>
-    <script src="{{asset('adminpage/assets/plugins/select2/js/select2.min.js')}}"></script>
-    <script
-        src="{{asset('adminpage/own-js/keuangan_page/penggajian/honorarium/honorarium_koreksi/daftar.js')}}"></script>
+    <script src="{{ asset('adminpage/assets/plugins/datatables/datatables.min.js') }}"></script>
+    <script src="{{ asset('adminpage/assets/plugins/select2/js/select2.min.js') }}"></script>
+    <script src="{{ asset('adminpage/own-js/keuangan_page/penggajian/honorarium/honorarium_koreksi/daftar.js') }}"></script>
 @endpush
