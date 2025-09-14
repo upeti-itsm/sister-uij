40a7aca854d3ac516b3edb0f3b3e0706d33bb8cd
Muhammad Mauribi <muhampedia.id@gmail.com>
2025-09-14 19:28:08 +0700

    Update class labels for clarity in rekapitulasi_absen_mengajar view

--- resources/views/dosen_page/akademik/rekapitulasi_absen_mengajar.blade.php
+++ resources/views/dosen_page/akademik/rekapitulasi_absen_mengajar.blade.php
@@ -35,7 +35,7 @@
             </div>
             <div class="card-footer p-1">
                 <div class="stats">
-                    <i class="fas fa-cloud-sun mr-2 ml-2"></i>Kelas Reguler Pagi
+                    <i class="fas fa-cloud-sun mr-2 ml-2"></i>Kelas Reguler
                 </div>
             </div>
         </div>
@@ -51,7 +51,7 @@
             </div>
             <div class="card-footer p-1">
                 <div class="stats">
-                    <i class="fas fa-cloud-moon mr-2 ml-2"></i>Kelas Reguler dan Hybrid
+                    <i class="fas fa-cloud-moon mr-2 ml-2"></i>Kelas Hybrid
                 </div>
             </div>
         </div>
